0007e5fd2bbb5a59d3b2ca915d8bb07227b9e2db
Ayub Ian <iayub.mia@gmail.com>
2023-03-01 12:25:16 +0300

    Added class and instance methods for models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,18 @@
 class Product < ActiveRecord::Base
     has_many :reviews
     has_many :users, through: :reviews
+
+    def leave_review(user,star_rating,comment)
+        
+    end
+
+    def print_all_reviews
+        self.reviews.map do |review|
+            review.print_review
+        end
+    end
+
+    def average_rating
+        self.reviews.average(:star_rating).to_f
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,23 @@ product2  = Product.create(name: Faker::Game.title, description: Faker::Game.gen
 
 review1 = Review.create(star_rating: rand(0..5), comment: "Loved the storyline")
 review2 = Review.create(star_rating: rand(0..5), comment: "Definitely an improvement from the last release")
+review3 = Review.create(star_rating: rand(0..5), comment: "This is a Monster Hunter style game. Naturally it's good")
+review4 = Review.create(star_rating: rand(0..5), comment: "I am surprised how good this game is")
+review5 = Review.create(star_rating: rand(0..5), comment: "Reminds me of the spastic combat of Fortnite")
+review6 = Review.create(star_rating: rand(0..5), comment: "Keeps me up all night. If you know what i mean.")
 
 user1.reviews << review2
 user2.reviews << review1
+user1.reviews << review4
+user2.reviews << review3
+user1.reviews << review5
+user2.reviews << review6
 
 product1.reviews << review1
 product2.reviews << review2
+product1.reviews << review3
+product2.reviews << review4
+product1.reviews << review5
+product2.reviews << review6
 
 puts "Done seeding!"
